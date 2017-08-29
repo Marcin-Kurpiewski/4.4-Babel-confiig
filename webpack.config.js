e48ba42a597742539319e4fb34a3cee10ad75255
Marcin-Kurpiewski <kurus.mk@gmail.com>
2017-08-29 18:20:17 +0200

    config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
-
 module.exports = {
     entry: './src/app.js',
     output: {
         path: './build',
         filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader"
+            }
+        ]
     }
 };
